0f4b792bee72e86b5bf3c965afe68b5f320f5d53
Ronnie-Crawford <113427947+Ronnie-Crawford@users.noreply.github.com>
2022-09-22 10:48:07 +0100

    Create READ_FILE.py
    For the READ_FILE process in blood_pressure_stats.nf pipeline.

---
+++ READ_FILE.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#Imports
+import sys
+import pandas as pd
+
+#Test standalone inputs
+#bpdf = pd.read_csv("blood_pressure.csv")
+#bpdf = pd.read_csv("$bp_file_in")
+
+#Create dataframe from input
+bpdf = pd.read_csv(sys.argv[1])
+
+#Extract data arrays
+systolic_pressures_out = bpdf["Systolic"]
+diastolic_pressures_out = bpdf["Diastolic"]
+bpms_out = bpdf["BPM"]
+
+#Write output to file
+bp_lists = open("bp_read_out.txt", "w")
+bp_lists.write(str(systolic_pressures_out))
+bp_lists.write(str(diastolic_pressures_out))
+bp_lists.write(str(bpms_out))
+bp_lists.close()
+
+#Test standalone outputs
+#print(systolic_pressures_out)
+#print(diastolic_pressures_out)
+#print(bpms_out)
