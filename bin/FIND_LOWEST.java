0d987685085b0ddaee79f671f97c20563b6328fc
rc30 <rc30@sanger.ac.uk>
2022-09-23 14:58:20 +0100

    Adding FIND_LOWEST process and associated java files

--- bin/FIND_LOWEST.java
+++ bin/FIND_LOWEST.java
@@ -1,4 +1,4 @@
-#!/usr/bin/env java
+//#!/usr/bin/env java
 
 //Imports
 import java.io.*;
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FindLowest
+public class FIND_LOWEST
 {
     //Test standalone input
     //static int[] systolic_pressures_in = {5,6,7,3,4,5,6,7,9};
@@ -16,22 +16,75 @@ public class FindLowest
 
     public static void main(String[] args)
     {
+        String systolic_pressures_string = "";
+        String diastolic_pressures_string = "";
+        String bpms_string = "";
+        
         //Import lists from input file
         try
         {
-				systolic_pressures_string = Files.readAllLines(Paths.get('bp_lists.txt')).get(0);
-                diastolic_pressures_string = Files.readAllLines(Paths.get('bp_lists.txt')).get(1);
-                bpms_string = Files.readAllLines(Paths.get('bp_lists.txt')).get(2);
+				systolic_pressures_string = Files.readAllLines(Paths.get("bp_lists.txt")).get(0);
+                diastolic_pressures_string = Files.readAllLines(Paths.get("bp_lists.txt")).get(1);
+                bpms_string = Files.readAllLines(Paths.get("bp_lists.txt")).get(2);
 		}
         catch (FileNotFoundException e)
         {
 			e.printStackTrace();
 		}
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
         
         //Convert lists from string to arrays
-        List<String> systolic_pressures_in = new ArrayList<String>(Arrays.asList(systolic_pressures_string.split(",")));
-        List<String> diastolic_pressures_in = new ArrayList<String>(Arrays.asList(diastolic_pressures_string.split(",")));
-        List<String> bpms_in = new ArrayList<String>(Arrays.asList(bpms_string.split(",")));
+        //List<String> systolic_pressures_in = new ArrayList<String>(Arrays.asList(systolic_pressures_string.split(",")));
+        //List<String> diastolic_pressures_in = new ArrayList<String>(Arrays.asList(diastolic_pressures_string.split(",")));
+        //List<String> bpms_in = new ArrayList<String>(Arrays.asList(bpms_string.split(",")));
+
+        String[] sys_frags = systolic_pressures_string.split(",");
+        String[] dias_frags = diastolic_pressures_string.split(",");
+        String[] bpm_frags = bpms_string.split(",");
+
+        int[] systolic_pressures_in = new int[sys_frags.length];
+        int[] diastolic_pressures_in = new int[dias_frags.length];
+        int[] bpms_in = new int[bpm_frags.length];
+
+
+        for (int i = 0; i < sys_frags.length; i++)
+        {
+            try
+            {
+                systolic_pressures_in[i] = Integer.parseInt(sys_frags[i]);
+            }
+            catch (NumberFormatException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < dias_frags.length; i++)
+        {
+            try
+            {
+                diastolic_pressures_in[i] = Integer.parseInt(dias_frags[i]);
+            }
+            catch (NumberFormatException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < bpm_frags.length; i++)
+        {
+            try
+            {
+                bpms_in[i] = Integer.parseInt(bpm_frags[i]);
+            }
+            catch (NumberFormatException e)
+            {
+                e.printStackTrace();
+            }
+        }
         
         //For each group of 3 systolic measures, return lowest
         int[] lowest_systolics = new int[(systolic_pressures_in.length / 3)];
@@ -104,11 +157,23 @@ public class FindLowest
         }
 
         //Write results to output file
-        PrintWriter writer = new PrintWriter("bp_lowest_out.txt", "UTF-8");
-        writer.println(Arrays.toString(lowest_systolics));
-        writer.println(Arrays.toString(lowest_diastolics));
-        writer.println(Arrays.toString(lowest_bpms));
-        writer.close();
+        try
+        {
+            PrintWriter writer = new PrintWriter("bp_lowest_out.txt", "UTF-8");
+            writer.println(Arrays.toString(lowest_systolics));
+            writer.println(Arrays.toString(lowest_diastolics));
+            writer.println(Arrays.toString(lowest_bpms));
+            writer.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        
 
         //Test standalone output
         //System.out.println(lowest_systolics.length);
--- blood_pressure_stats.nf
+++ blood_pressure_stats.nf
@@ -2,13 +2,14 @@
 
 //User sets file locations for Nextflow to use
 params.input_file = "$projectDir/blood_pressure.csv"
+params.find_lowest_file = "$projectDir/bin/FIND_LOWEST.jar"
+params
 
-// change
 //Main workflow
 workflow
 {
     READ_FILE(params.input_file)
-    FIND_LOWEST(READ_FILE.out)
+    FIND_LOWEST(READ_FILE.out, params.find_lowest_file)
     //FIND_AVERAGE(FIND_LOWEST.out)
     //OUTPUT(FIND_AVERAGE.out)
 }
@@ -33,14 +34,17 @@ process FIND_LOWEST
 {
     input:
         path ('bp_read_out.txt')
+        path find_lowest_file
 
     output:
         path ('bp_lowest_out.txt')
 
     script:
     """
-    FIND_LOWEST.java
+    java -jar ${find_lowest_file}
     """
+    //echo Main-Class: FIND_LOWEST>manifest.txt (BASH command to copy manifest.txt to .class file)
+    //jar cvfe FIND_LOWEST.jar FIND_LOWEST FIND_LOWEST.class (BASH command to add .class file to .jar file)
 }
 
 //Process to find average value of each lowest measurement - Python
@@ -63,17 +67,20 @@ process OUTPUT
 {
     input:
         path ('bp_average_out.txt')
-    
+
+    sys_average = $ sed '1!d' bp_average_out.txt
+    dias_average = $ sed '2!d' bp_average_out.txt
+    bpm_average = $ sed '3!d' bp_average_out.txt
+
     log.info
     """
         \
         B L O O D - P R E S S U R E - A N A L Y S E R
         =============================================
         File in: ${params.input_file}
-        Average low systolic blood pressure: ${sys_average}
-        Average low diastolic blood pressure: ${dias_average}
-        Average low bpm: ${bpm_average}
+        Average lowest systolic blood pressure: ${sys_average}
+        Average lowest diastolic blood pressure: ${dias_average}
+        Average lowest bpm: ${bpm_average}
         \
     """
 }
-
